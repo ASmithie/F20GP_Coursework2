b50652f5fbdac49f140c5ca0da6d8d1f29fad6fa
ASmithie <alecsmith_@outlook.com>
2022-03-06 21:55:36 +0000

    integrity working

--- F20GP_CW2/Assets/Scripts/Integrity.cs
+++ F20GP_CW2/Assets/Scripts/Integrity.cs
@@ -5,20 +5,15 @@ using UnityEngine;
 public class Integrity : MonoBehaviour
 {
     public float integrity = 10f;
-
     public float integrityDecrease;
-
-    public float force = 0f;
+    public float force = 0.1f;
     public float gravity = 9.81f;
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter(Collision col)
     {
-        integrityDecrease = force * gravity;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        float velocity = rb.velocity.magnitude;
+        integrityDecrease = force * gravity * Mathf.Abs(velocity);
         integrity-= integrityDecrease;
     }
 }
