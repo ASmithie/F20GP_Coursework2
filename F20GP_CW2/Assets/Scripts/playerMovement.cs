3fa7a2f037b661baf33107f80891a0bfb6cab7dd
Lewis Inches <li10@hw.ac.uk>
2022-03-14 11:07:35 +0000

    movement fixed

--- F20GP_CW2/Assets/Scripts/playerMovement.cs
+++ F20GP_CW2/Assets/Scripts/playerMovement.cs
@@ -5,29 +5,33 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     public CharacterController playerController;
+   
     public Transform cam;
-
-    public float speed = 6f;
+    
+    public float speed = 12f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
     // Update is called once per frame
     void Update()
     {
+        Rigidbody rb;
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
-        if(direction.magnitude >= 0.1f && focusBar.currentFocus > focusBar.costFocus)
+        if(direction.magnitude >= 0.1f /*&& focusBar.currentFocus > focusBar.costFocus*/)
         {
-            focusBar.currentFocus -= focusBar.costFocus;
+            //focusBar.currentFocus -= focusBar.costFocus;
 
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            playerController.Move(moveDir * speed * Time.deltaTime);
+            rb = GetComponent<Rigidbody>();
+            rb.AddForce(moveDir * speed);
+            //playerController.Move(moveDir * speed * Time.deltaTime);
 
         }
 
