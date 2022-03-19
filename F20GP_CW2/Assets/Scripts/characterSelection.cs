e57d3f1d9ed270cd17903f63575de3502cbc3aab
Lewis Inches <incheslewis@gmail.com>
2022-03-19 17:12:34 +0000

    character selection screen implemented and working

---
+++ F20GP_CW2/Assets/Scripts/characterSelection.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class characterSelection : MonoBehaviour
+{
+    private GameObject[] characterList;
+    private int characterIndex;
+    public GameObject thirdpersoncamera;
+    public CinemachineFreeLook vcam;
+
+    public GameObject canvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterList = new GameObject[transform.childCount];
+        for(int i = 0;i < transform.childCount; i++)
+        {
+            characterList[i] = transform.GetChild(i).gameObject;
+            characterList[i].SetActive(false);
+        }
+        characterList[0].SetActive(true);
+
+
+    }
+
+    public void selectLeft()
+    {
+        characterList[characterIndex].SetActive(false);
+        characterIndex--;
+        if (characterIndex < 0)
+        {
+            characterIndex = characterList.Length - 1;
+        }
+        characterList[characterIndex].SetActive(true);
+
+    }
+
+    public void selectRight()
+    {
+        characterList[characterIndex].SetActive(false);
+        characterIndex++;
+        if (characterIndex == characterList.Length)
+        {
+            characterIndex = 0;
+        }
+        characterList[characterIndex].SetActive(true);
+    }
+
+    public void confirm()
+    {
+        vcam.Follow = characterList[characterIndex].transform;
+        vcam.LookAt = characterList[characterIndex].transform;
+        playerMovement script = characterList[characterIndex].GetComponent<playerMovement>();
+        script.enabled = true;
+        thirdpersoncamera.SetActive(true);
+        canvas.SetActive(false);
+    }
+}
